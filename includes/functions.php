b9910832f449c5e1b691c07ead6b0d621a32213d
Ryoko Oka <ryooo.kooo.22@gmail.com>
2020-11-11 15:19:30 -0500

    adding PHP

--- includes/functions.php
+++ includes/functions.php
@@ -4,28 +4,30 @@
 
     $result = array();
 
-    function getAllUsers($conn) {
-        $query = "SELECT * FROM profdata";
+    function getAllUsers($conn)
+    {
+        $query = "SELECT * FROM profs";
 
         $runQuery = $conn->query($query);
 
-        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+        while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
             $result[] = $row;
         }
 
         //return $result;
-        echo (json_encode($result));
+        echo(json_encode($result));
     }
 
-    function getSingleUser($conn, $id) {
+    function getSingleUser($conn, $id)
+    {
         $query = "SELECT * FROM profs WHERE id=" . $id . "";
 
         $runQuery = $conn->query($query);
 
-        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+        while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
             $result[] = $row;
         }
 
         //return $result;
-        echo (json_encode($result));
+        echo(json_encode($result));
     }
